afd018b84d04301198d5f048c36c424d3a2ccbca
Rubayet-billah <rubayetbillah909@gmail.com>
2023-03-19 02:43:06 +0600

    slice add

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,15 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import apiSlice from "../features/api/apiSlice";
+import authSlice from "../features/auth/authSlice";
+import conversationsSlice from "../features/conversations/conversationsSlice";
+import messagesSlice from "../features/messages/messagesSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSlice,
+    conversations: conversationsSlice,
+    messages: messagesSlice,
   },
   devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
