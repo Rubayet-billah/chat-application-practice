ef29e9a80977115083d5282557d9fb57df821073
Rubayet-billah <rubayetbillah909@gmail.com>
2023-03-19 02:41:05 +0600

    add

--- src/app/store.js
+++ src/app/store.js
@@ -5,6 +5,7 @@ export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
 });
